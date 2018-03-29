0b5f7302fcd18e07e4490ede5aac22c357dea416
joseph.tompkins <joseph.tompkins@gmail.com>
2018-03-29 13:55:59 -0500

    Add `hashtable` class.
    Add `hashcode` function for computing a hash.

---
+++ hashtable_python/hashtable.py
@@ -0,0 +1,54 @@
+# Author: Joseph Tompkins
+#
+
+
+__all__ = ['hashcode', 'hashtable']
+
+
+def hashcode(key):
+    """
+    Computes a hash code for a given key value.
+    Uses the SDBM hash function.
+    """
+    hash = 0
+    _bytes = bytearray(bytes(key))
+    for byte in _bytes:
+        byte = byte << 8
+        hash = byte + (hash << 6) + (hash << 16) - hash
+    return hash
+
+class hashtable:
+    def __init__(self, table_sz=4096):
+        self.__table_sz = table_sz
+        self.__table = [None] * table_sz
+
+    def set(self, key, value):
+        """
+        Set the value of the item corresponding with the given key.\n
+        If the item does not yet exist, a new key/value pair will be created.
+        """
+        hash = hashcode(key)
+        index = hash % self.__table_sz
+        if self.__table[index] is None:
+            self.__table[index] = list()
+        if key not in [item[0] for item in self.__table[index]]:
+            self.__table[index].append((key, value))
+        else:
+            self.__table[index] = value
+
+    def get(self, key):
+        """Get the value of the item"""
+        hash = hashcode(key)
+        index = hash % self.__table_sz
+        for item in self.__table[index]:
+            if item[0] == key:
+                return item[1]
+        raise KeyError
+
+    def remove(self, key):
+        """Removes the item with the item with the given key."""
+        hash = hashcode(key)
+        index = hash % self.__table_sz
+        for item in self.__table[index]:
+            if item[0] == hash:
+                self.__table.pop(index)
